b96defb02e194882cc39b978e92219f40629b948
Ben Fields <me@benfields.net>
2016-05-11 12:05:02 +0100

    moving the sampling to postgres
    should reduce memory footprint

--- user_comment_spider.py
+++ user_comment_spider.py
@@ -20,7 +20,7 @@ import psycopg2
 from user import user
 
 # using this bit of magic to tunnel:
-# ssh -M -S my-ctrl-socket -fnNT -L 63333:localhost:5432 rg@<host db address>   
+# ssh -M -S my-ctrl-socket -fnNT -L 63333:localhost:5432 rg@<host db address>
 def create_or_open_db(connection_str="postgresql://rg:rg@localhost:63333/genius"):
     "setup the db connection, if needed create the tables"
     #close and reopen to deal with known if not exists bug
@@ -50,6 +50,14 @@ def known_users(curs):
     curs.execute("""SELECT id FROM users""")
     return curs.fetchall()
 
+def fetch_N_unseen_users(curs, max_id, n, presample=10000):
+    """grab 100 unseen users entire via sql for a smaller memory profile. takes a few seconds on a DO $10 vps, runtime scales to presample size"""
+    curs.execute("""SELECT possible_users.ids from users, (select generate_series(1,%s) as ids order by random() limit %s) as possible_users where possible_users.ids not in (select id from users) limit %s""", (max_id, presample, n))
+    users_to_visit = curs.fetchall()
+    curs.execute("""select count(*) from users""")
+    users_remaining = max_id - int(curs.fetchone()[0])
+    return users_to_visit, users_remaining
+
 def fetch_user_sets(curs, max_id):
     "gather user_sets"
     collected_users = known_users(curs)
@@ -58,28 +66,28 @@ def fetch_user_sets(curs, max_id):
 
 def store(curs, this_user):
     #insert users
-    curs.execute("""WITH upsert AS (UPDATE users SET username = %(name)s WHERE id = %(id)s RETURNING *) 
-                    INSERT INTO users (id, username) SELECT %(id)s,%(name)s WHERE NOT EXISTS (SELECT * FROM upsert)""", 
+    curs.execute("""WITH upsert AS (UPDATE users SET username = %(name)s WHERE id = %(id)s RETURNING *)
+                    INSERT INTO users (id, username) SELECT %(id)s,%(name)s WHERE NOT EXISTS (SELECT * FROM upsert)""",
                  {'id': this_user.rg_id, 'name': this_user.login})
     #insert comments
-    curs.executemany("""WITH upsert AS (UPDATE annotation 
-                                            SET song_id = %(song_link)s, comment = %(comment)s 
-                                            WHERE id = %(id)s RETURNING *) 
-                        INSERT INTO annotation (id, song_id, comment) SELECT %(id)s, %(song_link)s, %(comment)s WHERE NOT EXISTS (SELECT * FROM upsert)""", 
+    curs.executemany("""WITH upsert AS (UPDATE annotation
+                                            SET song_id = %(song_link)s, comment = %(comment)s
+                                            WHERE id = %(id)s RETURNING *)
+                        INSERT INTO annotation (id, song_id, comment) SELECT %(id)s, %(song_link)s, %(comment)s WHERE NOT EXISTS (SELECT * FROM upsert)""",
                      [{'id': a.rg_id,'song_link': a.song_link,'comment': a.text} for a in this_user.annotations])
     #tie comments to user
-    curs.executemany("""WITH upsert AS (UPDATE user_contributed_annotation 
-                                            SET user_id = %(user_id)s 
-                                            WHERE annotation_id = %(annotation_id)s RETURNING *) 
-                        INSERT INTO user_contributed_annotation (user_id, annotation_id) 
+    curs.executemany("""WITH upsert AS (UPDATE user_contributed_annotation
+                                            SET user_id = %(user_id)s
+                                            WHERE annotation_id = %(annotation_id)s RETURNING *)
+                        INSERT INTO user_contributed_annotation (user_id, annotation_id)
                             SELECT %(user_id)s, %(annotation_id)s WHERE NOT EXISTS (SELECT * FROM upsert)""",
                     [{'user_id':this_user.rg_id, 'annotation_id': a.rg_id} for a in this_user.annotations])
     #edit history
     for annotation in this_user.annotations:
-        curs.executemany("""WITH upsert AS (UPDATE annotation_history 
-                                                SET username = %(username)s, comment = %(comment)s 
-                                                WHERE annotation_id = %(annotation_id)s AND entry_time = %(entry_time)s RETURNING *) 
-                            INSERT INTO annotation_history (annotation_id, username, entry_time, comment) 
+        curs.executemany("""WITH upsert AS (UPDATE annotation_history
+                                                SET username = %(username)s, comment = %(comment)s
+                                                WHERE annotation_id = %(annotation_id)s AND entry_time = %(entry_time)s RETURNING *)
+                            INSERT INTO annotation_history (annotation_id, username, entry_time, comment)
                                 SELECT %(annotation_id)s, %(username)s, %(entry_time)s, %(comment)s WHERE NOT EXISTS (SELECT * FROM upsert)""",
                         [{'annotation_id':annotation.rg_id, 'username':a[0], 'entry_time':a[1], 'comment':a[2]} for a in annotation.history])
 
@@ -87,22 +95,19 @@ def main(argv=sys.argv):
     max_id = argv[-1]
     max_id = int(max_id)
     conn, curs = create_or_open_db()
-    users_remaining, collected_users = fetch_user_sets(curs, max_id)
-    sync_count = 0
-    while len(users_remaining) < max_id:
-        this_id = random.sample(users_remaining, 1)[0]
-        print "fetching contributions for user", this_id
-        this_user = user(rg_id=this_id)
-        this_user.get_annotations()
-        print "\tuser has", len(this_user.annotations), "annotations, storing..."
-        store(curs, this_user)
-        conn.commit()
-        users_remaining.remove(this_id)
-        collected_users.append(this_id)
-        sync_count += 1
-        if sync_count == 100:
-            users_remaining, collected_users = fetch_user_sets(curs, max_id)
-            sync_count = 0
+    users_remaining, some_users = fetch_N_unseen_users(curs, max_id, 100)
+    while len(users_remaining) < 0:
+        for (this_id,) in some_users:
+            print "fetching contributions for user", this_id
+            this_user = user(rg_id=this_id)
+            this_user.get_annotations()
+            print "\tuser has", len(this_user.annotations), "annotations, storing..."
+            store(curs, this_user)
+            conn.commit()
+            # users_remaining.remove(this_id)
+            # collected_users.append(this_id)
+
+        users_remaining, collected_users = fetch_N_unseen_users(curs, max_id, 100)
 
 
 
